156feeefaa684524797cfcb79259655dca13c31e
achatela <achatela@student.42.fr>
2023-10-10 16:49:15 +0200

    parsing v1

--- ExpertSystem.cpp
+++ ExpertSystem.cpp
@@ -17,14 +17,14 @@ ExpertSystem::ExpertSystem(std::string fileName)
     {
         if (isCommentOrEmpty(line))
             continue;
-        if (isInitialFact(line))
+        else if (isInitialFact(line))
         {
             if (initialized == true)
                 throw std::invalid_argument("Multiple initial facts lines !");
             // logic to set the characters to true TODO
             initialized = true;
         }
-        if (isQuery(line))
+        else if (isQuery(line))
         {
             if (initialized == false)
                 throw std::invalid_argument("Query line is before the initial fact line !");
@@ -33,8 +33,25 @@ ExpertSystem::ExpertSystem(std::string fileName)
             // logic to set the query characters in the _queries TODO
             queryInitialized = true;
         }
-
-        // std::cout << line << std::endl;
+        else // lines with facts
+        {
+            std::stringstream ss(line);
+            std::string word;
+            std::vector<std::string> tmp_vector;
+            while (ss >> word)
+            {
+                if (word[0] == '#')
+                    break;
+                tmp_vector.push_back(word);
+            }
+            addLineToRules(tmp_vector);
+        }
+    }
+    for (auto it : _rules)
+    {
+        std::cout << std::endl;
+        for (auto it2 : it)
+            std::cout << it2 << std::endl;
     }
 }
 
@@ -106,4 +123,125 @@ bool ExpertSystem::isInitialFact(std::string line) const
         }
     }
     return (true);
+}
+
+//
+
+void ExpertSystem::addLineToRules(std::vector<std::string> line)
+{
+    if (line.size() < 3)
+        throw std::invalid_argument("A line has invalid format !");
+
+    std::vector<Rules> toRules;
+    bool implicatorFound = false;
+
+    for (auto it : line) // still need to check multiple Operators in a row, same for characters TODO
+    {
+        Rules tmp;
+        if (isupper(it[0]))
+        {
+            if (it.size() != 1)
+                throw std::invalid_argument("A line has invalid format !");
+            if (implicatorFound == false)
+            {
+                tmp.isFact = true;
+                tmp.value = std::string(it);
+            }
+            else
+            {
+                tmp.isResult = true;
+                tmp.value = std::string(it);
+            }
+        }
+        else if (it[0] == '!')
+        {
+            if (it.size() != 2)
+                throw std::invalid_argument("A line has invalid format !");
+            if (isupper(it[1]))
+            {
+                if (implicatorFound == false)
+                {
+                    tmp.isFact = true;
+                    tmp.isTrue = false;
+                    tmp.value = std::string(it);
+                }
+                else
+                {
+                    tmp.isResult = true;
+                    tmp.isTrue = false;
+                    tmp.value = std::string(it);
+                }
+            }
+        }
+        else if (it.size() == 1 && (it[0] == '|' || it[0] == '+' || it[0] == '^'))
+        {
+            tmp.isOperator = true;
+            tmp.value = std::string(it);
+        }
+        else if (it == "=>" || it == "<=>")
+        {
+            if (implicatorFound == true)
+                throw std::invalid_argument("A line has two implicator !");
+            tmp.isImplicator = true;
+            tmp.value = std::string(it);
+            implicatorFound = true;
+        }
+        else
+        {
+            throw std::invalid_argument("A line has invalid format !");
+        }
+
+        // add a function to check if there is atleast one fact/implicator/result TODO
+        toRules.push_back(tmp);
+    }
+    checkLineValidity(toRules);
+    _rules.push_back(toRules);
+}
+
+void ExpertSystem::checkLineValidity(std::vector<Rules> toRules)
+{
+    bool hasFact = false;
+    bool hasImplicator = false;
+    bool hasResult = false;
+
+    int lastWas = 0;
+
+    if (toRules.size() < 3)
+        throw std::invalid_argument("A line has invalid format !");
+    if (toRules[0].isFact == false || toRules[toRules.size() - 1].isResult == false)
+        throw std::invalid_argument("A line has to start with a fact and end with a result !");
+    for (int i = 0; i < toRules.size(); i++)
+    {
+        Rules it = toRules[i];
+        if (hasFact == false && it.isFact == true)
+            hasFact = true;
+        else if (hasImplicator == false && it.isImplicator == true)
+            hasImplicator = true;
+        else if (hasResult == false && it.isResult == true)
+            hasResult = true;
+        if (it.isFact == true)
+        {
+            if (lastWas == 1)
+                throw std::invalid_argument("Two fact in a row !");
+            lastWas = 1;
+        }
+        else if (it.isOperator == true)
+        {
+            if (lastWas == 2)
+                throw std::invalid_argument("Two operators in a row !");
+            lastWas = 2;
+        }
+        else if (it.isResult == true)
+        {
+            if (lastWas == 3)
+                throw std::invalid_argument("Two results in a row !");
+            lastWas = 3;
+        }
+        else
+        {
+            lastWas = 0;
+        }
+    }
+    // if (hasFact != true || hasImplicator != true || hasResult != true)
+    //     throw std::invalid_argument("Every line needs at least one fact, implicator, and argument !");
 }
--- ExpertSystem.hpp
+++ ExpertSystem.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 #include <string>
 #include <map>
@@ -5,36 +6,59 @@
 #include <stdexcept>
 #include <fstream>
 #include <cctype>
+#include <sstream>
 
 // => is an implicator
 // ! is (not) true
 // D is the result
+struct Rules
+{
+    bool isFact = false;       // A B C... are facts
+    bool isOperator = false;   // + | ^ are operators
+    bool isImplicator = false; // => <=> are implicators
+    bool isResult = false;     // characters after implicator = result
+    bool isTrue = true;        // ! is (not) true
+    std::string value;
+
+    friend std::ostream &operator<<(std::ostream &os, const Rules &rules)
+    {
+        os << "value: " << rules.value << " ";
+
+        if (rules.isFact)
+            os << "is a fact ";
+        if (rules.isOperator)
+            os << "is an operator ";
+        if (rules.isImplicator)
+            os << "is an implicator ";
+        if (rules.isResult)
+            os << "is a result ";
+        if (rules.isTrue)
+            os << " and is true ";
+        else
+            os << " and is false ";
+
+        return os;
+    }
+};
+
 class ExpertSystem
 {
-
-    struct Rules
-    {
-        bool isFact;       // A B C... are facts
-        bool isOperator;   // + | ^ are operators
-        bool isImplicator; // => <=> are implicators
-        bool isResult;     // characters after implicator = result
-        bool isTrue;       // ! is (not) true
-
-        std::string value;
-    };
-
 private:
-    std::vector<Rules> _rules;
+    std::vector<std::vector<Rules>> _rules;
     std::map<char, int> _facts;
     std::vector<char> _queries;
 
 public:
     // Constructors/Destructors
-    ExpertSystem(std::string fileName);
+    ExpertSystem(std::string);
     ~ExpertSystem();
 
     // Parsing methods
     bool isCommentOrEmpty(std::string) const;
     bool isInitialFact(std::string) const;
     bool isQuery(std::string) const;
+
+    //
+    void addLineToRules(std::vector<std::string>);
+    void checkLineValidity(std::vector<Rules>);
 };
